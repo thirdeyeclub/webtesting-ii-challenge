26ce24eda6d73e7e6ec63c1589c4d9afafec0511
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-04-09 16:25:51 -0400

    json scripts fixed, now using this as primary branch

---
+++ app/src/App.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {render} from "react-testing-library"
+import App from './App';
+
+describe('APP', () => {
+  
+  describe('render()', () => {
+    
+    it('renders without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<App />, div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
+    
+    it('render?', () => {
+      render(<App />);
+    });
+    
+  });
+});
