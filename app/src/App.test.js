30bb289531b584ef59a5eb1ff2b7b624a79539d7
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-04-09 14:02:30 -0400

    set up testing

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {render , fireEvent} from "react-testing-library"
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('APP', () => {
+  
+  describe('render()', () => {
+    
+    it('renders without crashing', () => {
+      const div = document.createElement('div');
+      ReactDOM.render(<App />, div);
+      ReactDOM.unmountComponentAtNode(div);
+    });
+    
+    it('renders', () => {
+      render(<App />);
+    });
+    
+  });
 });
